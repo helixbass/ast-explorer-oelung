962826bc593e8f609e422ac42f8b48a0134d8864
Julian Rosse <julian@helixbass.net>
2026-01-02 19:27:41 -0500

    working on node

---
+++ src/ast.rs
@@ -0,0 +1,41 @@
+use smol_str::SmolStr;
+
+pub enum Value {
+    Node(Node),
+    Array(Vec<Value>),
+    Scalar(SmolStr),
+}
+
+pub struct Node {
+    pub type_: SmolStr,
+    pub range: Option<Range>,
+    pub children: Vec<NodeChild>,
+}
+
+pub struct NodeChild {
+    pub name: SmolStr,
+    pub value: Value,
+}
+
+pub type Offset = usize;
+
+#[derive(Copy, Clone, Debug)]
+pub struct Range {
+    pub start: Location,
+    pub end: Location,
+}
+
+#[derive(Copy, Clone, Debug)]
+pub enum Location {
+    JustOffset(Offset),
+    OffsetAndPosition { offset: Offset, position: Position },
+    JustPosition(Position),
+}
+
+#[derive(Copy, Clone, Debug)]
+pub struct Position {
+    /// 0-based
+    pub line: usize,
+    /// 0-based
+    pub column: usize,
+}
