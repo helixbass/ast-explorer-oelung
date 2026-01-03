4239917c266a5e45755c26101ff8d9c7dc0ca64d
Julian Rosse <julian@helixbass.net>
2026-01-03 11:16:49 -0500

    working on node

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,19 @@
+use crossterm::event::{Event, EventStream, KeyCode};
+use oelung::{soft, Renderer};
+use oelung_lantern::{generate_sender, mpsc::Sender};
+use tokio::sync::mpsc::channel;
+use tokio_stream::StreamExt;
+
 use ast_explorer_oelung::{syn::Parser, AstExplorer, Parse};
 
-fn main() -> Result<(), anyhow::Error> {
+#[tokio::main]
+async fn main() -> Result<(), anyhow::Error> {
+    let mut renderer = Renderer::try_new()?;
+
+    let (sender, mut receiver) = channel::<World>(100);
+
+    listen_to_crossterm_events(CrosstermSender::from(sender.clone()));
+
     let text = r#"
 const TIPS: &[&str] = &[
     "Click on any AST node with a '+' to expand it",
@@ -20,5 +33,46 @@ pub fn print_tips() {
     let parser = Parser::new();
     let explorer = AstExplorer::new(parser.parse(text)?);
 
+    render_screen(&mut renderer, &explorer)?;
+
+    while let Some(world) = receiver.recv().await {
+        match world {
+            World::Crossterm(Event::Key(key)) if key.code == KeyCode::Char('q') => {
+                break;
+            }
+            _ => {}
+        }
+    }
+
     Ok(())
+}
+
+fn render_screen(renderer: &mut Renderer, explorer: &AstExplorer) -> Result<(), anyhow::Error> {
+    renderer.render(soft! {
+      %Text
+        children => [
+          %explorer
+          %Text " (hit q to quit)"
+        ]
+    })?;
+
+    Ok(())
+}
+
+enum World {
+    Crossterm(Event),
+}
+
+generate_sender!(World, Crossterm, Event);
+
+fn listen_to_crossterm_events(sender: CrosstermSender) {
+    tokio::spawn(async move {
+        let mut event_stream = EventStream::new();
+
+        while let Some(Ok(event)) = event_stream.next().await {
+            sender.send(event).await;
+        }
+
+        panic!("kill everything")
+    });
 }
