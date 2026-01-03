123cff211ef1c4c7a765b090a0667ecd295e4605
Julian Rosse <julian@helixbass.net>
2026-01-03 10:04:07 -0500

    working on node

---
+++ src/ast_panel.rs
@@ -0,0 +1,70 @@
+use oelung::{anyhow, soft, Component, ComponentInterface, Grid};
+use smallvec::SmallVec;
+
+use crate::ast;
+
+const SPACES_PER_NESTING_LEVEL: usize = 4;
+
+pub struct AstPanel {
+    pub tree: ast::Node,
+}
+
+impl<'a> ComponentInterface for &'a AstPanel {
+    fn render(&self, _grid: Grid) -> Result<Component<'_>, anyhow::Error> {
+        Ok(soft! {
+            %Node::new(&self.tree, 0)
+        })
+    }
+}
+
+pub struct Node<'a> {
+    pub node: &'a ast::Node,
+    pub nesting_level: usize,
+}
+
+impl<a> Node<'a> {
+    pub fn new(node: &'a ast::Node, nesting_level: usize) -> Self {
+        Self {
+            node,
+            nesting_level,
+        }
+    }
+}
+
+impl<'a> ComponentInterface for Node<'a> {
+    fn render(&self, _grid: Grid) -> Result<Component<'_>, anyhow::Error> {
+        Ok(soft! {
+            %FlexColumn
+                children => {
+                    [
+                        soft! {
+                            %Text
+                              children => [
+                                %InitialSpaces::new(self.nesting_level)
+                                %Text &self.node.type_
+                                %Text " {"
+                              ]
+                        },
+                    ].into_iter()
+                        .chain(
+                            self.node.children.iter().map(|child| {
+                                Ok(soft! {
+                                    %NodeChild::new(child, self.nesting_level)
+                                })
+                            }).collect::<Result<SmallVec<_, 10>, _>>()?
+                        )
+                        .chain(
+                            [soft! {
+                                %Text
+                                  children => [
+                                    %InitialSpaces::new(self.nesting_level)
+                                    %Text "}"
+                                  ]
+                            }]
+                        )
+                        .collect()
+                }
+                flex_grow => 1
+        })
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 pub mod ast;
+mod ast_panel;
 mod error;
 pub mod syn;
 
 pub use ast::{Location, Node, NodeChild, Offset, Parse, Position, Range, Value};
+pub use ast_panel::AstPanel;
 pub use error::Error;
 pub use syn::Parser;
